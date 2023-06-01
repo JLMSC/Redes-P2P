f872cbc7fce6a5c2bd34f0b9ea879e3608e3172a
JLMSC <reecaptcha@gmail.com>
2023-06-01 13:31:05 -0300

    Ajustado o arquivo main.py

--- source/main.py
+++ source/main.py
@@ -1,10 +1,15 @@
 """Arquivo principal."""
 
+from typing import Any
+
+from graph import Network
 from reader import read_json_file
 
 def main(file_path: str) -> None:
     """Função principal."""
-    read_json_file(file_path=file_path)
+    data_read: Any = read_json_file(file_path=file_path)
+    network: Network = Network(data_info=data_read)
+    network.check_network()
 
 if __name__ == '__main__':
     # main(file_path=input('Informe o caminho do arquivo de entrada: '))
