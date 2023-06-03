a0585d7f8ce200dabf8e8c297839cb9f37e59cea
JLMSC <reecaptcha@gmail.com>
2023-06-03 20:40:41 -0300

    Main.py ajustado para uso indefinido do programa.

--- source/main.py
+++ source/main.py
@@ -11,6 +11,12 @@ def main(file_path: str) -> None:
     network: Network = Network(data_info=data_read)
     network.check_network()
 
+    # TODO: try except!!!!! (se não os raise vão parar o programa.)
+    while True:
+        print('\n' * 50)
+        print('Pressione [Ctrl + C] para sair.\n')
+        network.run_search()
+        # TODO: Dps de executar um algoritmo, pedir confirmação de entrada p/ continuar.
+
 if __name__ == '__main__':
-    # main(file_path=input('Informe o caminho do arquivo de entrada: '))
     main(file_path='source/input.json')
