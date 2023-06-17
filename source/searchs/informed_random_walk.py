dab3575978c7ae76663797e874037331fc23abf6
JLMSC <reecaptcha@gmail.com>
2023-06-17 16:10:14 -0300

    Correção na recursividade (condição de parada).

--- source/searchs/informed_random_walk.py
+++ source/searchs/informed_random_walk.py
@@ -46,54 +46,56 @@ def informed_random_walk(node: Any, resource: str, ttl: int) -> None:
         """
         nonlocal messages_count, resource_found, visited_nodes
 
-        # Recurso foi encontrado!
-        if resource in node.resources:
-            print(
-                f'\nO recurso {resource} FOI' +\
-                f' encontrado no nó {node.node_id}!' +\
-                '\n\t`--> Caminho: ' +\
-                ' -> '.join(n.node_id for n in path) +\
-                f'\n\t`--> Qntd. de mensagens trocadas: {messages_count}' +\
-                f'\n\t`--> Qntd. de nós envolvidos: {len(visited_nodes)}'
-            )
-            resource_found = True
-            # Atualiza o cache dos nós da origem até o nó com o recurso.
-            for node_path in path:
-                node_path.add_cache(node=node, resource=resource)
+        # Se o recurso já foi encontrado!
+        if not resource_found:
+            # Recurso foi encontrado!
+            if resource in node.resources:
+                print(
+                    f'\nO recurso {resource} FOI' +\
+                    f' encontrado no nó {node.node_id}!' +\
+                    '\n\t`--> Caminho: ' +\
+                    ' -> '.join(n.node_id for n in path) +\
+                    f'\n\t`--> Qntd. de mensagens trocadas: {messages_count}' +\
+                    f'\n\t`--> Qntd. de nós envolvidos: {len(visited_nodes)}'
+                )
+                resource_found = True
+                # Atualiza o cache dos nós da origem até o nó com o recurso.
+                for node_path in path:
+                    node_path.add_cache(node=node, resource=resource)
 
-        # Marca o nó atual como visitado.
-        visited_nodes.add(node)
+            # Marca o nó atual como visitado.
+            visited_nodes.add(node)
 
-        # Cria uma cópia e randomiza a ordem dos vizinhos.
-        neighbors: list[Any] = list(node.neighbors)
-        random_neighbors: list[Any] = list(neighbors)
-        shuffle(x=random_neighbors)
+            # Cria uma cópia e randomiza a ordem dos vizinhos.
+            neighbors: list[Any] = list(node.neighbors)
+            random_neighbors: list[Any] = list(neighbors)
+            shuffle(x=random_neighbors)
 
-        for neighbor in random_neighbors:
-            # Ignora os nós já visitados e que tenham TTL > 0
-            if ttl > 0:
-                messages_count += 1
-                if neighbor not in visited_nodes:
-                    # Vai direto para o nó que tem o recurso.
-                    if neighbor.know_resource(resource=resource):
-                        # O nó que contém o recurso (cache).
-                        target_node: Any = neighbor.get_node_by_resource(
-                            resource=resource
-                        )
-                        recursive_informed_walk(
-                            node=target_node,
-                            resource=resource,
-                            ttl=ttl - 1,
-                            path=path + [target_node]
-                        )
-                    # Caso contrário, visita, aleatoriamente, os vizinhos.
-                    else:
-                        recursive_informed_walk(
-                            node=neighbor,
-                            resource=resource,
-                            ttl=ttl - 1,
-                            path=path + [neighbor]
-                        )
+            for neighbor in random_neighbors:
+                # Ignora os nós já visitados e que tenham TTL > 0
+                if ttl > 0:
+                    messages_count += 1
+                    if neighbor not in visited_nodes:
+                        # Vai direto para o nó que tem o recurso.
+                        if neighbor.know_resource(resource=resource):
+                            # O nó que contém o recurso (cache).
+                            target_node: Any = neighbor.get_node_by_resource(
+                                resource=resource
+                            )
+                            recursive_informed_walk(
+                                node=target_node,
+                                resource=resource,
+                                ttl=ttl - 1,
+                                path=path + [target_node]
+                            )
+                        # Caso contrário, visita, aleatoriamente, os vizinhos.
+                        else:
+                            recursive_informed_walk(
+                                node=neighbor,
+                                resource=resource,
+                                ttl=ttl - 1,
+                                path=path + [neighbor]
+                            )
 
     recursive_informed_walk(node=node, resource=resource, ttl=ttl, path=[node])
 
